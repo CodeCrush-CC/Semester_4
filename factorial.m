1b3b0caaafb4b3d3fee0f56505dfd398ebc05776
Tushar Malhotra <tusharmalhotra494@gmail.com>
2020-01-29 23:53:00 -0800

    commit 1

---
+++ factorial.m
@@ -0,0 +1,13 @@
+
+function[] = fact() ;
+
+n = input('Enter the number:') ;
+
+s = 1 ;
+
+for i=1:n 
+  s =s*i ;
+endfor
+fprintf('%d', s) ;
+fprintf('\nTushar Malhotra \n Roll Number: 42513202718') ;
+end
