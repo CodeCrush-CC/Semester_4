ebc8492bb0d8e4c67a6cbd7d191c51a96bf1cadc
Tushar Malhotra <tusharmalhotra494@gmail.com>
2020-01-30 00:08:43 -0800

    New files

---
+++ fibonacci_No_3.m
@@ -0,0 +1,17 @@
+
+function[] = fib() ;
+
+a=0; 
+b=1 ;
+
+n = input('Enter the number of terms:') ;
+
+for i=1:n 
+  fprintf('%d\t',a) ;
+  c=a+b ;
+  a=b ;
+  b=c ;
+  
+  endfor
+fprintf('\nTushar Malhotra \n\n Roll Number: 42513202718') ;
+end
